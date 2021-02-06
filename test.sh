fc0e33e82aaca4ac58bf9e1db0791cbac0e005dd
eric.marchand <eric.marchand@4d.com>
2021-02-06 22:06:21 +0100

    Enhance example script to launch 4D with - a database in $HOME/Bisect - a file created on error by the base if test failed (because 4D do not allow QUIT with exit code)

--- test.sh
+++ test.sh
@@ -3,15 +3,31 @@
 version=$1
 root=$2
 
+myBase=$HOME/Bisect # customize it
+
 path=$root/$version/release/INTL/mac_INTL_64/4D_INTL_x86_64.zip
 
+unzipPath=$(mktemp -d)
+
 if [[ -f "$path" ]]; then
-    if [ "$version" -gt "241902" ]; then
-        exit 1
+    unzip -q '$path' $unzipPath
+
+    if [[ -f "$unzipPath/4D/4D.app/Contents/MacOS/4D" ]]; then
+        $unzipPath/4D/4D.app/Contents/MacOS/4D --headless --dataless -s "$myBase/Project/builder.4DProject" # run onStart of this project, must auto QUIT
+
+        # failed if a file has been created in resources (for install an error handler and create the file if an assert occurs)
+        # if 4D return error code, it will be better, just return its code with $? 
+        if [[ -f "$myBase/Resources/error" ]]; then
+            rm -Rf $unzipPath
+            exit 1
+        else
+            rm -Rf $unzipPath
+            exit 0
+        fi
     else
-        exit 0
+        >&2 echo "No 4D command in zip $path"
+        exit 125 # skip
     fi
 else
     exit 125 # skip
 fi
-
